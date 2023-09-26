444d46763c7a8e1ee7b684c651b32d964c7596bd
Saurabh Mirajkar <mirajkar46@gmail.com>
2023-09-26 13:27:42 +0530

    create a expense model

---
+++ lib/models/expense.dart
@@ -0,0 +1,16 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String amount;
+  final String title;
+  final DateTime date;
+}
